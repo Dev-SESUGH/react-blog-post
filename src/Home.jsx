871be026e138738a2d9be30aea4126a2d7514edf
simeon amiase <amiase97@gmail.com>
2023-05-18 14:49:00 +0100

    my firt commit on this blog

---
+++ src/Home.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import BLogList from "./BlogLIst";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log(useEffect);
+  });
+
+  return (
+    <div className="home">
+      <BLogList blogs={blogs} handleDelete={handleDelete} />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="nav-text">blog post</h1>
+      <div className="links">
+        <a href="#">HOME</a>
+        <a href="#">NEW BLOG</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
