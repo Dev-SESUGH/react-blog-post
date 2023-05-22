ff0560aeb30f1aa61b2f2b579e9660e5d8616620
simeon amiase <amiase97@gmail.com>
2023-05-22 17:22:23 +0100

    error message updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,10 +9,16 @@ const Home = () => {
     setTimeout(() => {
       fetch(" http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data), setisPending(false);
+        })
+        .catch((err) => {
+          console.log(err.message);
         });
     }, 300);
   }, []);
